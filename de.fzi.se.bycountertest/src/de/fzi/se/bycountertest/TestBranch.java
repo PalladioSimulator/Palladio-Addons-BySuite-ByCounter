e6cea4807ecbaeda212e838963eef667a0db85ff
s_fschre <s_fschre>
2012-02-04 13:49:00 +0000

    einige testfälle auf expectation framework angepasst
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/BySuite/ByCounter/trunk@14057 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.fzi.se.bycountertest/src/de/fzi/se/bycountertest/TestBranch.java
+++ de.fzi.se.bycountertest/src/de/fzi/se/bycountertest/TestBranch.java
@@ -3,16 +3,17 @@
  */
 package de.fzi.se.bycountertest;
 
-/**Test for branches (return value)
+/**
+ * Test for branches (return value)
+ * 
  * @author groenda
- *
  */
 public class TestBranch {
 	public int process(int input) {
-		if (input >0 && input < 10) {
+		if (input > 0 && input < 10) {
 			input++;
-		} 
-		if (input >= 10){
+		}
+		if (input >= 10) {
 			input *= 1;
 		}
 		return input;
