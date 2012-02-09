3ccf0871dd44d898580bcdfa8bd238a1efdec5ac
s_fschre <s_fschre>
2012-02-09 17:53:40 +0000

    removed wrong assertion
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/BySuite/ByCounter/trunk@14117 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.ByCounter/src/de/uka/ipd/sdq/ByCounter/test/framework/expectations/SectionExpectation.java
+++ de.uka.ipd.sdq.ByCounter/src/de/uka/ipd/sdq/ByCounter/test/framework/expectations/SectionExpectation.java
@@ -58,8 +58,6 @@ public class SectionExpectation {
 	 *          The line number range of the new section. If the range is unknown, this should be <code>null</code>.
 	 */
 	protected SectionExpectation(final int sectionNumber, final LineNumberRange range) {
-//		assert sectionNumber >= 0 : "sectionNumber has to be greater or equal zero.";
-
 		this.sectionNumber = sectionNumber;
 		this.range = range;
 		this.opcodeExpectations = new HashMap<Integer, Long>();
